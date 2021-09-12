d4a48c3d4eb103acd1b1a1891d99591fdc2ca13f
chris <felix.christopher.e@gmail.com>
2021-09-11 20:57:26 -0700

    login validated

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,18 +1,18 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users');
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 
-router.get('/set-token-cookie', asyncHandler(async(req, res) => {
-  const user = await User.findOne({
-    where: {
-      username: 'Demo-lition'
-    },
-  })
-  setTokenCookie(res, user);
-  return res.json({ user });
-}));
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 
+
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 
 module.exports = router;
