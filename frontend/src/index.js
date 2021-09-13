9e3d357e2fdfeef8833b299377ba2858843043af
chris <felix.christopher.e@gmail.com>
2021-09-12 20:44:16 -0700

    this sucks

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,8 +6,11 @@ import './index.css';
 import App from './App';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
+
+
 
 
 if (process.env.NODE_ENV !== 'production') {
@@ -15,6 +18,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
