f3e1a0e49647df10b669e99e59baddd44fb6a80e
chris <felix.christopher.e@gmail.com>
2021-09-15 15:19:01 -0700

    almost finished with store

--- frontend/src/store/properties.js
+++ frontend/src/store/properties.js
@@ -1,32 +1,68 @@
-// import { csrfFetch } from './csrf';
+import { csrfFetch } from './csrf';
 
 // Define action types as constants
 const SET_PROPERTIES = 'properties/setProperties'
 const CREATE_PROPERTY = 'properties/createProperties'
+const GET_ONE = 'properties/oneProperty'
+const CHANGE_PROPERTY = 'properties/changeProperty'
+const DELETE_PROPERTY = 'properties/deleteProperty'
 // define action creators
+
+//get all properties
 const setProperties = (properties) => ({
     type: SET_PROPERTIES,
     properties,
 });
 
+//get one property by id
+const getOneProperty = (property) => ({
+    type: GET_ONE,
+    property
+});
+
+//Create a new property
 const createOneProperty = (properties) => ({
     type: CREATE_PROPERTY,
     properties,
 });
+
+//Update an existing property
+const changeOneProperty = (property) => ({
+    type: CHANGE_PROPERTY,
+    property,
+});
+
+//Delete a property by ID
+const deleteOneProperty = (property) => ({
+    type: DELETE_PROPERTY,
+    property,
+})
+
 //define thunks
+
+//get all properties
 export const getProperties = () => async (dispatch) => {
   const res = await fetch('/api/properties', )
   const properties = await res.json();
   dispatch(setProperties(properties))
 };
 
+//get one property by id
+export const getProperty = (id) => async (dispatch) => {
+    const response = await fetch(`/api/properties/${id}`)
+
+    if (response.ok) {
+        const property = await response.json();
+        dispatch(getOneProperty(property));
+    };
+};
+
+//Create a new property
 export const createProperty = (data) => async (dispatch) => {
-    console.log(data)
-    const response = await fetch(`/api/properties`, {
+    // console.log(data)
+    const response = await csrfFetch(`/api/properties`, {
         method:'post',
-        headers: {
-            'Content-Type': 'application/json',
-        },
+        headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(data),
     });
 
@@ -34,7 +70,30 @@ export const createProperty = (data) => async (dispatch) => {
         const property = await response.json();
         dispatch(createOneProperty(property));
         return property
-    }
+    };
+};
+
+// Update an existing property
+export const changeProperty = (data) => async (dispatch) => {
+    const response = await csrfFetch(`/api/properties/${data.id}`, {
+        method:'put',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data),
+    });
+    if(response.ok) {
+        const property = await response.json();
+        dispatch(changeOneProperty(property));
+        return property
+    };
+};
+
+export const deleteProperty = (data) => async (dispatch) => {
+        const response = await csrfFetch(`/api/properties/${data.id}`, {
+            method: 'delete'
+        })
+        if(response.ok) {
+            dispatch(deleteOneProperty(data));
+        };
 };
 
 // define an initial state
@@ -47,14 +106,33 @@ const propertiesReducer = (state = initialState, action) => {
             const newState = {}
             action.properties.forEach(property => newState[property.id] = property);
             return newState;
+        case GET_ONE: {
+            const newState = {
+                ...state,
+                [action.properties.id]: action.properties
+            };
+                return newState;
+            };
 
         case CREATE_PROPERTY: {
+            const newState = {
+                ...state,
+                [action.properties.id]: action.properties
+            };
+                return newState;
+            };
+        case CHANGE_PROPERTY: {
                 const newState = {
-                    ...state,
-                    [action.properties.id]: action.properties
-                };
+                ...state,
+                [action.properties.id]: action.properties
+             };
                 return newState;
             }
+        case DELETE_PROPERTY: {
+            const newState = {...state};
+            delete newState[action.data.id]
+            return newState;
+        }
             default:
                 return state;
     }
